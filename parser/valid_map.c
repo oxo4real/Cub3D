09af66a96a61fed87e4d422ebb417cac8669105a
oxo4real <abduredoxo@hotmail.com>
2025-03-12 22:27:10 +0000

    added valid_map.c

--- parser/valid_map.c
+++ parser/valid_map.c
@@ -6,7 +6,7 @@
 /*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 19:27:22 by aaghzal           #+#    #+#             */
-/*   Updated: 2025/03/12 20:43:01 by aaghzal          ###   ########.fr       */
+/*   Updated: 2025/03/12 22:26:51 by aaghzal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ bool	ft_check_walls(char **map, int map_height)
 		{
 			if ((j > (ft_strlen(map[i - 1]) - 1)
 					|| j > (ft_strlen(map[i + 1]) - 1))
-				&& map[i][j] != '1')
+				&& map[i][j] != '1' && map[i][j] != ' ')
 				return (false);
 			if (map[i][j] == ' ' && !check_space_surroundings(map, i, j))
 				return (false);
