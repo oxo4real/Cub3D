c0d09e5e56aac3cee1aca42e9e4c503666575157
oxo4real <abduredoxo@hotmail.com>
2025-03-17 03:57:02 +0000

    finished the ray caster

---
+++ raycaster/draw_col.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_col.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 20:25:10 by aaghzal           #+#    #+#             */
+/*   Updated: 2025/03/17 03:44:59 by aaghzal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "raycaster.h"
+#include "utils.h"
+
+static double	ft_map(int px, t_info *inf);
+
+void	draw_col(t_info *inf, int px)
+{
+	double	ray_len;
+	double	angle;
+	int		ceiling_end;
+	int		i;
+	double	vlen;
+	double	hlen;
+
+	angle = ft_map(px, inf);
+	vlen = vray_len(inf, angle);
+	hlen = hray_len(inf, angle);
+	ray_len = ft_min(vlen, hlen) * cos(inf->player.dir - angle);
+	ceiling_end = (int)((HEIGHT / 2.0) - HEIGHT / ray_len);
+	i = 0;
+	while (i < HEIGHT)
+	{
+		if (i < ceiling_end)
+			mlx_put_pixel(inf->frame, px, i, inf->_c);
+		else if (i < HEIGHT - ceiling_end)
+		{
+			if (ray_len == hlen * cos(inf->player.dir - angle))
+				mlx_put_pixel(inf->frame, px, i, 0xFF0000FF);
+			else
+				mlx_put_pixel(inf->frame, px, i, 0xAA0000FF);
+		}
+		else
+			mlx_put_pixel(inf->frame, px, i, inf->_f);
+		i++;
+	}
+}
+
+static double	ft_map(int px, t_info *inf)
+{
+	double	ratio;
+	double	angle;
+
+	ratio = (double)px / (double)WIDTH;
+	angle = FOV * ratio + (inf->player.dir - (FOV / 2.0));
+	angle = fmod(angle, 2 * M_PI);
+	if (angle < 0)
+		angle += 2 * M_PI;
+	return (angle);
+}
---
+++ raycaster/vray_len.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vray_len.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaghzal <aaghzal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/16 21:36:33 by aaghzal           #+#    #+#             */
+/*   Updated: 2025/03/17 03:47:55 by aaghzal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "types.h"
+#include "utils.h"
+#include <limits.h>
+#include <math.h>
+
+typedef struct s_ray
+{
+	double	yo;
+	double	xo;
+	double	ry;
+	double	rx;
+}			t_ray;
+
+static void	init_vars(t_ray *ray, t_info *inf, double angle);
+static bool	ray_in_map(t_ray *ray, t_info *inf);
+static bool	player_in_wall(t_info *inf);
+
+double	vray_len(t_info *inf, double angle)
+{
+	t_ray	ray;
+	int		i;
+
+	init_vars(&ray, inf, angle);
+	if (player_in_wall(inf))
+		return (sqrt(pow(inf->player.y - ray.ry, 2) + pow(inf->player.x
+					- ray.rx, 2)));
+	i = 0;
+	while (i < DOF)
+	{
+		if ((ray_in_map(&ray, inf))
+			&& inf->map[(int)ray.ry][(int)ray.rx] == '1')
+			return (sqrt(pow(inf->player.y - ray.ry, 2) + pow(inf->player.x
+						- ray.rx, 2)));
+		ray.ry += ray.yo;
+		ray.rx += ray.xo;
+		i++;
+	}
+	return (INT_MAX);
+}
+
+static void	init_vars(t_ray *ray, t_info *inf, double angle)
+{
+	if (angle >= 3 * M_PI_2 || angle <= M_PI_2)
+	{
+		ray->rx = ceil(inf->player.x);
+		ray->ry = (inf->player.x - ray->rx) * -tan(angle) + inf->player.y;
+		ray->xo = 1;
+		ray->yo = -ray->xo * -tan(angle);
+	}
+	else if (angle <= 3 * M_PI_2 && angle >= M_PI_2)
+	{
+		ray->rx = floor(inf->player.x) - 0.00001;
+		ray->ry = (inf->player.x - ray->rx) * -tan(angle) + inf->player.y;
+		ray->xo = -1;
+		ray->yo = -ray->xo * -tan(angle);
+	}
+}
+
+static bool	ray_in_map(t_ray *ray, t_info *inf)
+{
+	return (ray->ry < inf->height && ray->ry >= 0 && ray->rx >= 0
+		&& ray->rx < (int)ft_strlen(inf->map[(int)ray->ry]));
+}
+
+static bool	player_in_wall(t_info *inf)
+{
+	return (inf->player.y < inf->height && inf->player.y >= 0
+		&& inf->player.x >= 0
+		&& inf->player.x < ft_strlen(inf->map[(int)inf->player.y])
+		&& inf->map[(int)inf->player.y][(int)inf->player.x] == '1');
+}
